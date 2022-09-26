9ba9468906321af961cdae9f18a7ecae5f36d669
coleyrockin <coleyrockin@aol.com>
2022-09-26 17:58:30 -0500

    Controllers, thought-controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,4 @@
-const { reduce } = require("benchmark");
 const { Thought, User } = require("../models");
-const { db } = require("../models/User");
 
 //Get All Thoughts
 const thoughtController = {
@@ -77,4 +75,56 @@ const thoughtController = {
   },
 
   //Delete Thought
-}
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: "No thought with this id" });
+        }
+        return User.findOneAndUpdate(
+          { thoughts: params.id },
+          { $pull: { thoughts: params.id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "Thought created but no user with this id" });
+        }
+        res.json({ message: "Thought successfully deleted" });
+      })
+      .catch((err) => res.json(err));
+  },
+
+  //Add Reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this id" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  //Delete Reaction
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = thoughtController;
