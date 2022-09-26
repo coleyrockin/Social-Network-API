f4daf1ff0b3858d3963b011f7d04ec557faa8cc3
coleyrockin <coleyrockin@aol.com>
2022-09-26 16:57:14 -0500

    User.js code

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    usernme: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: "Username Required!",
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: "Username Required!",
+      match: [/.+@.+\..+/]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
