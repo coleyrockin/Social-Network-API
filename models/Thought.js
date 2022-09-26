60a43592083b8e219ca4cb6c704680e4ad7189c5
coleyrockin <coleyrockin@aol.com>
2022-09-26 17:13:10 -0500

    Thought.js Models

--- models/Thought.js
+++ models/Thought.js
@@ -29,3 +29,39 @@ const ReactionSchema = new Schema(
     id: false,
   }
 );
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Thought is Required",
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      dafault: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
